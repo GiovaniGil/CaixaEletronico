4dba9e88fd14437cb014f75ea8f2aed5eaea103e
GiovaniGil <giovani_paulino@hotmail.com>
2016-04-04 18:50:05 -0300

    Tentando corrigir problemas com css em prod.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,7 @@ CaixaEletronico::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
-  config.assets.precompile += %w( *.css *.js )
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
